ea4b654e3bf497bb5959d8f09e33d6c13281bca5
PhaniGudipati <125386823+PhaniGudipati@users.noreply.github.com>
2023-06-09 08:38:09 +0530

    Add files via upload

---
+++ WrapsExx.java
@@ -0,0 +1,18 @@
+class WrapsExx{
+	public static void main(String args[]){
+		System.out.println("enter the 1st number:" + args[0]);
+		System.out.println("Enter the 2nd number :" + args[1]);
+		int c=args[0]+args[1];
+		System.out.println(c);
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
